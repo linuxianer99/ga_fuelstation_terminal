bb728a3a32ce0523e15a5478308d5203210cc3c4
Timo Bruderek <timo@timoshome.org>
2023-09-28 23:13:46 +0200

    Added dirty flag to version

--- auto_firmware_version.py
+++ auto_firmware_version.py
@@ -2,16 +2,16 @@ import subprocess
 
 Import("env")
 
-def get_firmware_specifier_build_flag():
-    ret = subprocess.run(["git", "describe"], stdout=subprocess.PIPE, text=True) #Uses only annotated tags
-    #ret = subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE, text=True) #Uses any tags
-    build_version = ret.stdout.strip()
-    build_flag = "-D AUTO_VERSION=\\\"" + build_version + "\\\""
-    print ("Firmware Revision: " + build_version)
-    return (build_flag)
+#def get_firmware_specifier_build_flag():
+#    ret = subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE, text=True) #Uses only annotated tags
+#    #ret = subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE, text=True) #Uses any tags
+#    build_version = ret.stdout.strip()
+#    build_flag = "-D AUTO_VERSION=\\\"" + build_version + "\\\""
+#    print ("Firmware Revision: " + build_version)
+#    return (build_flag)
 
 def get_firmware_specifier_build_version():
-    ret = subprocess.run(["git", "describe"], stdout=subprocess.PIPE, text=True) #Uses only annotated tags
+    ret = subprocess.run(["git", "describe", "--dirty"], stdout=subprocess.PIPE, text=True) #Uses only annotated tags
     #ret = subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE, text=True) #Uses any tags
     build_version = ret.stdout.strip()
     return (build_version)
